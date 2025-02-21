0c0a930ff10f978de26810fe56bfe0f4be737201
Wiona Glaenzer <wiona.glaenzer@icloud.com>
2025-02-21 14:59:47 +0100

    make filepaths changeable in the config file

--- linclust.sh
+++ linclust.sh
@@ -1,8 +1,10 @@
-mkdir linclust
-cd linclust
+#!/bin/bash
+linclust_dir="$1"
+sequences_fasta="$2"
+mkdir $linclust_dir
+cd $linclust_dir
 echo "path: $(pwd)"
-mmseqs createdb ../outputs/sequences.fasta antibody_DB
+mmseqs createdb $sequences_fasta antibody_DB
 mmseqs linclust --cov-mode 1 -c 0.9 --min-seq-id 0.9 antibody_DB antibody_DB_clu tmp
 mmseqs createsubdb antibody_DB_clu antibody_DB antibody_DB_clu_rep
 mmseqs convert2fasta antibody_DB_clu_rep antibody_DB_clu_rep.fasta
-
