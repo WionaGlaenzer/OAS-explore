a8aa985d1797e62b2444e9263114beeb71f4981d
Wiona Glaenzer <wiona.glaenzer@icloud.com>
2025-04-14 14:38:05 +0200

    improve pre tokenization

--- standalone_training.sh
+++ standalone_training.sh
@@ -9,24 +9,32 @@ mkdir -p /cluster/project/reddy/wglaenzer/final_training_testing_val_data/Soto-H
 # Use semicolons to separate commands.
 # Escape '$' for variables that should be expanded inside the job (e.g., \$RPORT, \$CUDA_HOME).
 # Escape '"' where nested quotes are needed inside the wrapped string (e.g., \" for echo, \" for endpoint).
-COMMAND_TO_WRAP="echo 'CUDA_HOME is set to: \$CUDA_HOME'; \
+COMMAND_TO_WRAP="#!/bin/bash
+echo 'CUDA_HOME is set to: \$CUDA_HOME'; \
 echo \"Using rendezvous port: \$RPORT\"; \
 export WANDB_DIR='${WANDB_BASE_DIR}'; \
-torchrun --nproc_per_node=auto --rdzv-backend=c10d --rdzv-endpoint=localhost:26262 \
+. /etc/profile; \
+module load eth_proxy; \
+module load stack/2024-06 gcc/12.2.0; \
+module load python/3.11.6 cuda/12.1.1 ninja/1.11.1; \
+export CUDA_HOME=/cluster/software/stacks/2024-06/spack/opt/spack/linux-ubuntu22.04-x86_64_v3/gcc-12.2.0/cuda-12.1.1-5znnrjb5x5xr26nojxp3yhh6v77il7ie; \
+source /cluster/project/reddy/wglaenzer/environments/training/bin/activate
+python -m torch.distributed.run --nproc_per_node 6 \
   train_model.py \
   /cluster/project/reddy/wglaenzer/final_training_testing_val_data/Soto-HIP1/training.txt \
-  /cluster/project/reddy/wglaenzer/final_training_testing_val_data/Soto-HIP1/validation.txt \
   /cluster/project/reddy/wglaenzer/final_training_testing_val_data/Soto-HIP1/test.txt \
+  /cluster/project/reddy/wglaenzer/final_training_testing_val_data/Soto-HIP1/validation.txt \
   /cluster/scratch/wglaenzer/huggingface_cache \
   /cluster/project/reddy/wglaenzer/final_training_testing_val_data/Soto-HIP1/model \
   HIP1_model \
   /cluster/project/reddy/wglaenzer/final_training_testing_val_data/Soto-HIP1/model/.done \
   /cluster/home/wglaenzer/Coding/plm_training_pipeline/assets/antibody-tokenizer \
-  /cluster/home/wglaenzer/Coding/plm_training_pipeline/assets/deepspeed_config.json"
+  /cluster/home/wglaenzer/Coding/plm_training_pipeline/assets/deepspeed_config.json
+  --deepspeed /cluster/home/wglaenzer/Coding/plm_training_pipeline/assets/deepspeed_config.json "
 
 # Submit the job using sbatch and the constructed command string
-sbatch -A es_reddy -n 1 --cpus-per-task=1 --mem-per-cpu=20048 --gpus=1 --gres=gpumem:1gb \
---time=01:00:00 --mem-per-cpu=100g --job-name="train_HIP1" \
+sbatch -A es_reddy -n 1 --cpus-per-task=6 --gpus=6 --gres=gpumem:20gb --mem-per-cpu=20048  \
+--time=96:00:00  --job-name="train_HIP1" \
 --tasks=1 \
 --wrap="$COMMAND_TO_WRAP"
 
