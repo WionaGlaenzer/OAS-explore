8783433247b20fa83ca2eb09238191d7dc1a1c5f
Wiona Glaenzer <wiona.glaenzer@icloud.com>
2025-04-22 17:00:44 +0200

    name change:

--- submit_testing_job.sh
+++ submit_testing_job.sh
@@ -13,7 +13,7 @@ export TOKENIZER_PATH="${ASSETS_DIR}/antibody-tokenizer"
 export CACHE_DIR="/cluster/scratch/wglaenzer/huggingface_cache"
 
 # --- Slurm Configuration ---
-JOB_NAME="train_${MODEL_NAME}"
+JOB_NAME="test_${MODEL_NAME}"
 
 # --- Environment Modules & CUDA ---
 echo "Loading modules..."
