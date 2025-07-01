3ec8374fe73c591a57b5825ac7d88bf137aaf15c
Wiona Glaenzer <wiona.glaenzer@icloud.com>
2025-07-01 10:28:42 +0200

    delete unnecessary file

--- model_assessment/plot_model_performance.py
+++ model_assessment/plot_model_performance.py
@@ -3,34 +3,34 @@ import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
 
-OAS_model_on_HIP1 = 0.475341796875
-OAS_model_on_HIP2 = 0.428955078125
-OAS_model_on_HIP3 = 0.42523783445358276
-OAS_model_on_OAS = 0.3640836775302887
+OAS_model_on_HIP1 = 0.475634765625
+OAS_model_on_HIP2 = 0.427783203125
+OAS_model_on_HIP3 = 0.4259765625
+OAS_model_on_OAS = 0.364111328125
 OAS_model_soto = (OAS_model_on_HIP1 + OAS_model_on_HIP2 + OAS_model_on_HIP3) /3
 
-Soto_model_on_OAS = 0.47304069995880127
-Soto_model_on_HIP1 = 0.3652347922325134
-Soto_model_on_HIP2 = 0.3340586721897125
-Soto_model_on_HIP3 = 0.3303837478160858
+Soto_model_on_OAS = 0.47255859375
+Soto_model_on_HIP1 = 0.365185546875
+Soto_model_on_HIP2 = 0.3337890625
+Soto_model_on_HIP3 = 0.330810546875
 Soto_model_soto = (Soto_model_on_HIP1 + Soto_model_on_HIP2 + Soto_model_on_HIP3) /3
 
 HIP1_model_on_OAS = 0.486328125
-HIP1_model_on_HIP1 = 0.31787109375
-HIP1_model_on_HIP2 = 0.437744140625
-HIP1_model_on_HIP3 = 0.4296875
+HIP1_model_on_HIP1 = 0.317822265625
+HIP1_model_on_HIP2 = 0.436669921875
+HIP1_model_on_HIP3 = 0.429736328125
 HIP1_model_on_soto = (HIP1_model_on_HIP1 + HIP1_model_on_HIP2 + HIP1_model_on_HIP3) /3
 
 HIP2_model_on_OAS = 0.490966796875
-HIP2_model_on_HIP1 = 0.4853515625
-HIP2_model_on_HIP2 = 0.300537109375
-HIP2_model_on_HIP3 = 0.4228515625
+HIP2_model_on_HIP1 = 0.485400390625
+HIP2_model_on_HIP2 = 0.29970703125
+HIP2_model_on_HIP3 = 0.422998046875
 HIP2_model_on_soto = (HIP2_model_on_HIP1 + HIP2_model_on_HIP2 + HIP2_model_on_HIP3) /3
 
 HIP3_model_on_OAS = 0.490234375
-HIP3_model_on_HIP1 = 0.492431640625
-HIP3_model_on_HIP2 = 0.43408203125
-HIP3_model_on_HIP3 = 0.300537109375
+HIP3_model_on_HIP1 = 0.492724609375
+HIP3_model_on_HIP2 = 0.4328125
+HIP3_model_on_HIP3 = 0.301220703125
 HIP3_model_on_soto = (HIP3_model_on_HIP1 + HIP3_model_on_HIP2 + HIP3_model_on_HIP3) /3
 
 
@@ -47,7 +47,7 @@ sns.heatmap(loss_df, annot=True, cmap="viridis_r", cbar_kws={'label': 'Loss'}, x
 plt.title("Model performance on different test sets")
 plt.xlabel("Test data")
 plt.ylabel("Training data")
-plt.savefig("loss_different_test_sets.pdf")
+plt.savefig("loss_different_test_sets_means.pdf")
 
 
 
