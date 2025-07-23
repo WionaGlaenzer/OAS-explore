6c6fc209a91bfc2fdb0f878bed0e2b6bb3d6a94f
Wiona Glaenzer <wiona.glaenzer@icloud.com>
2025-07-23 15:45:02 +0200

    code cleanup

--- pipeline/pre_tokenize.py
+++ pipeline/pre_tokenize.py
@@ -4,6 +4,7 @@ from tqdm import tqdm
 import os
 import sys
 
+# Process arguments
 training_file = sys.argv[1]
 val_file = sys.argv[2]
 test_file = sys.argv[3]
@@ -11,6 +12,7 @@ cache_dir = sys.argv[4]
 out_dir = sys.argv[5]
 tokenizer_path = sys.argv[6]
 
+# Load tokenizer and construct output path
 tokenizer = RobertaTokenizer.from_pretrained(tokenizer_path)
 tokenized_path = os.path.join(out_dir, "tokenized")
 os.makedirs(tokenized_path, exist_ok=True)
@@ -27,6 +29,13 @@ max_length = 150
 buffer_size = 1000000  # How many examples to batch before writing
 
 def tokenize_and_save(split_name, file_path):
+    """
+    Reads in one split of the data and saves it at the specified path.
+
+    Args:
+        split_name (str): Name of the dataset split (train, eval, test).
+        file_path (str): Path to the dataset file.
+    """
     print(f"Streaming and tokenizing: {split_name}")
     
     stream = load_dataset("text", data_files={split_name: file_path}, cache_dir=cache_dir, split=split_name, streaming=True)
